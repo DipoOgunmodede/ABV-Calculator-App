2613a08b53f693e8007fc69005a6dd3ebf5910ed
Dipo Ogunmodede <dipoogunmodede@gmail.com>
2022-06-15 20:56:34 +0100

    trying to create spirit values component

--- src/components/ABVCalculator.vue
+++ src/components/ABVCalculator.vue
@@ -2,23 +2,11 @@
   <form id="abvCalc" class="max-w-48 flex flex-col text-center">
     <fieldset class="text-center flex justify-center">
       <label for="drink-name" class="font-bold">Drink name</label>
-      <input type="text" id="drink-name" class="ml-4 text-blue-800" v-model="this.drink.drinkOptions.drinkName" placeholder="Name" />
+      <input type="text" id="drink-name" class="ml-4 text-blue-800" v-model="this.drink.drinkOptions.drinkName"
+        placeholder="Name" />
     </fieldset>
-    <fieldset class="spiritValuesContainer" v-for="(spirit, index) in drink.spirits" :key="index">
-
-      <input type="text" />
-      <label :for="`total-drink-abv-${index}`" class="w-full flex flex-col my-4">{{ spirit.name }} ABV in %</label>
-      <input :id="`total-drink-abv-${index}`"
-        class="m-4 p-4 text-3xl text-center focus-within:outline-dashed focus:outline-green-500 focus:outline-4"
-        type="number" min="0" v-model="spirit.spiritABV" placeholder="40" />
-
-      <label :for="`total-alcohol-quantity-${index}`" class="w-full flex flex-col my-4">{{ spirit.name }} quantity in
-        ml</label>
-      <input :id="`total-alcohol-quantity-${index}`"
-        class="m-4 p-4 text-3xl text-center focus-within:outline-dashed focus:outline-green-500 focus:outline-4"
-        type="number" min="0" v-model="spirit.spiritQuantity" placeholder="25" />
-    </fieldset>
-
+    <spirit-values name="Vodka" :abv="40" :quantity="25"></spirit-values>
+        <spirit-values name="Gin" :abv="40" :quantity="25"></spirit-values>
     <fieldset v-for="(mixer, index) in drink.mixers" :key="index">
       <label :for="`mixer-total-quantity-${index}`" class="w-full flex flex-col my-4">Mixer quantity in ml</label>
       <input :id="`mixer-total-quantity-${index}`"
@@ -48,104 +36,95 @@
 </template>
 
 <script>
+import SpiritValues from './SpiritValues.vue';
 export default {
   data() {
     return {
       drink: {
         spirits: [
           {
-            name: 'Vodka',
-            spiritABV: 37.5,
-            spiritQuantity: 0
+            name: "Vodka",
+            spiritABV: 60,
+            spiritQuantity: 40
           },
-          {
-            name: 'Gin',
-            spiritABV: 40,
-            spiritQuantity: 0
-          },
-          {
-            name: 'Rum',
-            spiritABV: 40,
-            spiritQuantity: 0
-          },
-          {
-            name: 'Whiskey',
-            spiritABV: 40,
-            spiritQuantity: 0
-          },
-          {
-            name: 'Tequila',
-            spiritABV: 40,
-            spiritQuantity: 0
-          },
+          // {
+          //     name: "Gin",
+          //     spiritABV: 40,
+          //     spiritQuantity: 0
+          // },
+          // {
+          //     name: "Rum",
+          //     spiritABV: 40,
+          //     spiritQuantity: 0
+          // },
+          // {
+          //     name: "Whiskey",
+          //     spiritABV: 40,
+          //     spiritQuantity: 0
+          // },
+          // {
+          //     name: "Tequila",
+          //     spiritABV: 40,
+          //     spiritQuantity: 0
+          // },
           // {
           //   name: 'Absinthe',
           //   spiritABV: 89.9,
           //   spiritQuantity: 0
           // },
-           {
-            name: 'Triple Sec',
-            spiritABV: 40,
-            spiritQuantity: 0
-          }
+          // {
+          //     name: "Triple Sec",
+          //     spiritABV: 40,
+          //     spiritQuantity: 0
+          // }
         ],
         mixers: [{
-          name: 'Coke',
+          name: "Coke",
           mixerQuantity: 330,
         }],
         drinkOptions: {
-          drinkName: 'Miscellaneous',
+          drinkName: "Miscellaneous",
           mixersHaveIce: false,
         },
-
       },
       appOptions: {
         numberOfDecimals: 2,
       }
-
-    }
-  }
-  ,
+    };
+  },
   methods: {
     numberToFractionOfOne(num) {
-      return num / 100
+      return num / 100;
     },
     fractionToPercentage(num) {
-      return num * 100
+      return num * 100;
     },
-
     roundFloatingPoint(num, fixed) {
       // eslint-disable-next-line
-      var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
+      var re = new RegExp("^-?\\d+(?:.\\d{0," + (fixed || -1) + "})?");
       return num.toString().match(re)[0];
     }
   },
-
   computed: {
     calculateMixersTotalQuantity() {
       return this.drink.mixers.reduce((acc, mixer) => {
-        return acc + mixer.mixerQuantity
-      }, 0)
+        return acc + mixer.mixerQuantity;
+      }, 0);
     },
-
     calculateSpiritsTotalQuantity() {
       return this.drink.spirits.reduce((acc, spirit) => {
-        return acc + spirit.spiritQuantity
-      }, 0)
+        return acc + spirit.spiritQuantity;
+      }, 0);
     },
-
     calculateTotalAlcoholQuantityToFloatingPoint() {
-
       let totalAlcoholQuantity = this.drink.spirits.reduce((acc, spirit) => {
-        return acc + (spirit.spiritQuantity * this.numberToFractionOfOne(spirit.spiritABV))
+        return acc + (spirit.spiritQuantity * this.numberToFractionOfOne(spirit.spiritABV));
       }, 0);
       return totalAlcoholQuantity;
-
     },
     calculateTotalAlcoholQuantity() {
       return this.roundFloatingPoint(this.calculateTotalAlcoholQuantityToFloatingPoint, this.appOptions.numberOfDecimals);
     },
-
     calculateTotalDrinkQuantity() {
       return this.drink.drinkOptions.mixersHaveIce ?
         ((this.calculateMixersTotalQuantity * 1.2) + this.calculateSpiritsTotalQuantity) :
@@ -153,63 +132,48 @@ export default {
     },
     calculateTotalDrinkABVToFloatingPoint() {
       let abvToFloatingPointPercentage = this.fractionToPercentage(this.calculateTotalAlcoholQuantityToFloatingPoint / this.calculateTotalDrinkQuantity);
-      return abvToFloatingPointPercentage
+      return abvToFloatingPointPercentage;
     },
     calculateTotalDrinkABV() {
       return this.roundFloatingPoint(this.calculateTotalDrinkABVToFloatingPoint, this.appOptions.numberOfDecimals);
     },
-
     computedABVColourClasses() {
-      const totalDrinkABV = this.calculateTotalDrinkABVToFloatingPoint
-
-      if (
-        totalDrinkABV < 2.8
-      ) {
-        return 'text-green-400'
+      const totalDrinkABV = this.calculateTotalDrinkABVToFloatingPoint;
+      if (totalDrinkABV < 2.8) {
+        return "text-green-400";
       }
-
-      if (
-        totalDrinkABV >= 2.8 &&
-        totalDrinkABV < 6
-      ) {
-        return 'text-green-600'
+      if (totalDrinkABV >= 2.8 &&
+        totalDrinkABV < 6) {
+        return "text-green-600";
       }
-
-      if (
-        totalDrinkABV < 10
-      ) {
-        return 'text-amber-400'
+      if (totalDrinkABV < 10) {
+        return "text-amber-400";
       }
       if (totalDrinkABV >= 10) {
-        return 'text-red-600'
+        return "text-red-600";
       }
-
-      return ''
+      return "";
     },
     computeTotalAlcoholColourClasses() {
-      const totalAlcoholQuantity = this.calculateTotalAlcoholQuantityToFloatingPoint
+      const totalAlcoholQuantity = this.calculateTotalAlcoholQuantityToFloatingPoint;
       if (totalAlcoholQuantity < 25) {
-        return 'text-green-400'
+        return "text-green-400";
       }
-      if (
-        totalAlcoholQuantity >= 25 &&
-        totalAlcoholQuantity < 50
-      ) {
-        return 'text-green-600'
+      if (totalAlcoholQuantity >= 25 &&
+        totalAlcoholQuantity < 50) {
+        return "text-green-600";
       }
-
-      if (
-        totalAlcoholQuantity >= 50 &&
-        totalAlcoholQuantity < 75
-      ) {
-        return 'text-amber-400'
+      if (totalAlcoholQuantity >= 50 &&
+        totalAlcoholQuantity < 75) {
+        return "text-amber-400";
       }
       if (totalAlcoholQuantity >= 75) {
-        return 'text-red-600'
+        return "text-red-600";
       }
-      return ''
+      return "";
     }
-  }
+  },
+  components: { SpiritValues }
 }
 
 </script>
---
+++ src/components/SpiritValues.vue
@@ -0,0 +1,34 @@
+<template>
+<fieldset class="spiritValuesContainer">
+      <!-- <label :for="`spirit-${index}`" class="text-center">Spirit name</label>        -->
+      <!-- <input type="text" :id="`spirit-${index}`" :value="name" /> -->
+      <label :for="`total-drink-abv-${index}`" class="w-full flex flex-col my-4">{{ name }} ABV in %</label>
+      <input :id="`total-drink-abv-${index}`"
+        class="m-4 p-4 text-3xl text-center focus-within:outline-dashed focus:outline-green-500 focus:outline-4"
+        type="number" min="0" :value="ABV"  />
+
+      <label :for="`total-alcohol-quantity-${index}`" class="w-full flex flex-col my-4">{{ name }} quantity in
+        ml</label>
+      <input :id="`total-alcohol-quantity-${index}`"
+        class="m-4 p-4 text-3xl text-center focus-within:outline-dashed focus:outline-green-500 focus:outline-4"
+        type="number" min="0" />
+    </fieldset>
+</template>
+
+<script>
+    export default{
+        props: ['name', 'abv', 'quantity'],
+        data(){
+            return{
+                    spirit:
+                        {
+                            spiritName:'Gin',
+                            spiritABV:40,
+                            spiritQuantity:100
+                        },
+                    
+                }
+            }
+        }
+    
+</script>
